54fb9dd63789f862dd08f5a437be61401b0da28f
Erick Jones <erjonesokt@gmail.com>
2017-10-04 23:43:03 -0300

    Printing out node positions of a compressed txt file
    Signed-off-by: Erick Jones <erjonesokt@gmail.com>

--- rdzp.cpp
+++ rdzp.cpp
@@ -1,29 +1,67 @@
 #include <zip.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
 
 int main()
 {
   //Open the ZIP archive
   int err = 0;
-  zip *z = zip_open("foo.zip", 0, &err);
+  zip *z = zip_open("files/test.zip", 0, &err);
 
-  //Search for the file of given name
-  const char *name = "file.txt";
-  struct zip_stat st;
-  zip_stat_init(&st);
-  zip_stat(z, name, 0, &st);
+  //zip_int64_t num_entries = zip_get_num_entries(z, 0);
 
-  //Alloc memory for its uncompressed contents
-  char *contents = new char[st.size];
+  //for (zip_uint64_t i = 0; i < (zip_uint64_t)num_entries; i++) {
 
-  //Read the compressed file
-  zip_file *f = zip_fopen(z, name, 0);
-  zip_fread(f, contents, st.size);
-  zip_fclose(f);
+    //const char *name = zip_get_name(z, i, 0);
 
-  //close the archive
-  zip_close(z);
+    const char *name = "test.txt";
 
-  //Do something with the contents
-  //delete allocated memory
-  delete[] contents;
+    struct zip_stat st; 
+    zip_stat_init(&st);
+    zip_stat(z, name, 0, &st);
+
+    //Alloc memory for its uncompressed contents
+    char *contents = new char[st.size];
+
+    //Read the compressed file
+    zip_file *f = zip_fopen(z, name, 0);
+    zip_fread(f, contents, st.size);
+    zip_fclose(f);
+    zip_close(z);
+
+    if(!ofstream(name).write(contents, st.size)){
+      cerr << "Error writing file" << endl;
+      return EXIT_FAILURE;
+    }
+    //delete allocated memory
+    delete[] contents;
+
+    char letter;
+    ifstream reader(name);
+    if(!reader){
+
+      cout << "Error opening file"  << endl;
+      return EXIT_FAILURE;
+
+    } else {
+
+      for(int i = 0; !reader.eof(); i++){
+        reader.get(letter);
+        if(letter == '<'){
+          std::cout << "node beggining at position: " << i << std::endl;
+        }
+
+        if(letter == '>'){
+          std::cout << "node ending at position: " << i << std::endl;
+        }
+        //cout << letter;
+        //writer << letter << "2";
+      }
+      reader.close();
+    }
+  //}
+  return 0;
 }
